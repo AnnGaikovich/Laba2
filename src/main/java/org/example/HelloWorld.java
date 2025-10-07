9f02ba3e0485f1ab9792b3bd59d5e1bdb58e445b
Anna_Gaikovich <anngaikovich@gmail.com>
2025-10-07 22:16:13 +0300

    Внесла изменения для ветки conflict_1

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Привет, мир!!!");
+        System.out.println("Привет, мир!");
         System.out.println("Добавляю новую строку для ветки feature.");
 
         System.out.println("...");
