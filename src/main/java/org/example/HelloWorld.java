8b14d25bbe1486c1f2132cb41cc26b171c7ada98
Anna_Gaikovich <anngaikovich@gmail.com>
2025-10-07 21:36:05 +0300

    Добавила строку для ветки bugfix

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Привет, мир!, Hello Vitally!");
         System.out.println("Добавляю новую строку для ветки feature.");
+
         System.out.println("...");
     }
 }
