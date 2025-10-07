1ffd7682f72d394763900f6fefed1b9b85d999e0
Anna_Gaikovich <anngaikovich@gmail.com>
2025-10-07 21:30:09 +0300

    Добавила строку для ветки main

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -3,5 +3,7 @@ package org.example;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Привет, мир!, Hello Vitally!");
+        System.out.println("Добавляю новую строку для ветки feature.");
+        System.out.println("Добавляю новую строку для ветки main.");
     }
 }
