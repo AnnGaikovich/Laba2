da5cbedd128020ba87178dd6eaa1e968335b6a57
Anna_Gaikovich <anngaikovich@gmail.com>
2025-10-07 21:27:42 +0300

    Добавила строку для ветки feature

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -3,5 +3,6 @@ package org.example;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Привет, мир!, Hello Vitally!");
+        System.out.println("Добавляю новую строку для ветки feature.");
     }
 }
