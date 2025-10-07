4a0c7537021b4aeb051a3ed0238e2c8e0770de68
Anna_Gaikovich <anngaikovich@gmail.com>
2025-10-07 21:41:23 +0300

    Я внесла изменения на ветке main.

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Привет, мир!, Hello Vitally!");
         System.out.println("Добавляю новую строку для ветки feature.");
+        System.out.println("...");
     }
 }
