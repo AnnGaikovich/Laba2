30ef8008719d77ad3d94ccbcab6a38ccda972561
Anna_Gaikovich <anngaikovich@gmail.com>
2025-10-07 22:17:13 +0300

    Внесла изменения для ветки main

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Привет, мир!, Hello Vitally!");
+        System.out.println("Приветствую, мир!");
         System.out.println("Добавляю новую строку для ветки feature.");
 
         System.out.println("...");
